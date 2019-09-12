0a54759024e0559527b5bf6c265cb2904d3fdf7c
Lucas Lebihan <Lucas.Lebihan@ensimag.grenoble-inp.fr>
2019-09-12 09:59:44 +0200

    new CMake and new BlackScholesModel skeleton

--- src/BlackScholesModel.cpp
+++ src/BlackScholesModel.cpp
@@ -11,10 +11,147 @@
 
 #include "BlackScholesModel.h"
 
-void BlackScholesModel::asset(float **&market) {
-
-    //TODO
-
+/**
+ * \brief Getter de l'attribut \refitem size_
+ *
+ * @return l'attribut \refitem size_
+ *
+ */
+int BlackScholesModel::getSize() const {
+    return size_;
 }
 
-//TODO
+/**
+ * \brief Getter de l'attribut \refitem r_
+ *
+ * @return l'attribut \refitem r_
+ *
+ */
+double BlackScholesModel::getR() const {
+    return r_;
+}
+
+/**
+ * \brief Getter de l'attribut \refitem rho_
+ *
+ * @return l'attribut \refitem rho_
+ *
+ */
+double BlackScholesModel::getRho() const {
+    return rho_;
+}
+
+/**
+ * \brief Getter de l'attribut \refitem sigma_
+ *
+ * @return l'attribut \refitem sigma_
+ *
+ */
+PnlVect *BlackScholesModel::getSigma() const {
+    return sigma_;
+}
+
+/**
+ * \brief Getter de l'attrivut \refitem spot_
+ *
+ * @return l'attribut \refitem sigma_
+ *
+ */
+PnlVect *BlackScholesModel::getSpot() const {
+    return spot_;
+}
+
+/**
+ * \brief Setter de l'attribut \refitem size_
+ *
+ * @param size le nouveau nombre d'actifs du modèle, \refitem size_
+ *
+ */
+void BlackScholesModel::setSize(int size) {
+    size_ = size;
+}
+
+/**
+ * \brief Setter de l'attribut \refitem r_
+ *
+ * @param r le nouveau taux d'intérêt du ZC, \refitem r_
+ */
+void BlackScholesModel::setR(double r) {
+    r_ = r;
+}
+
+/**
+ * \brief Setter de l'attribut \refitem rho_
+ *
+ * @param rho la nouvelle corrélation du modèle, \refitem rho_
+ *
+ */
+void BlackScholesModel::setRho(double rho) {
+    rho_ = rho;
+}
+
+/**
+ * \brief Setter de l'attribut \refitem sigma_
+ *
+ * @param sigma le nouveau vecteur de volatilités du modèle, \refitem sigma_
+ *
+ */
+void BlackScholesModel::setSigma(PnlVect *sigma) {
+    sigma_ = sigma;
+}
+
+/**
+ * \brief Setter de l'attribut \refitem spot_
+ *
+ * @param spot le nouveau vecteur des spots initiaux du modèle, \refitem spot_
+ *
+ */
+void BlackScholesModel::setSpot(PnlVect *spot) {
+    spot_ = spot;
+}
+
+/**
+ * Génère une trajectoire du modèle et la stocke dans path
+ *
+ * @param[out] path contient une trajectoire du modèle.
+ * C'est une matrice de taille (nbTimeSteps+1) x d
+ * @param[in] T  maturité
+ * @param[in] nbTimeSteps nombre de dates de constatation
+ */
+void BlackScholesModel::asset(PnlMat *path, double T, int nbTimeSteps, PnlRng *rng) {
+    //TODO
+}
+
+/**
+ * Calcule une trajectoire du modèle connaissant le
+ * passé jusqu' à la date t
+ *
+ * @param[out] path  contient une trajectoire du sous-jacent
+ * donnée jusqu'à l'instant t par la matrice past
+ * @param[in] t date jusqu'à laquelle on connait la trajectoire.
+ * t n'est pas forcément une date de discrétisation
+ * @param[in] nbTimeSteps nombre de pas de constatation
+ * @param[in] T date jusqu'à laquelle on simule la trajectoire
+ * @param[in] past trajectoire réalisée jusqu'a la date t
+ */
+void BlackScholesModel::asset(PnlMat *path, double t, double T, int nbTimeSteps, PnlRng *rng, const PnlMat *past) {
+    //TODO
+}
+
+/**
+ * Shift d'une trajectoire du sous-jacent
+ *
+ * @param[in]  path contient en input la trajectoire
+ * du sous-jacent
+ * @param[out] shift_path contient la trajectoire path
+ * dont la composante d a été shiftée par (1+h)
+ * à partir de la date t.
+ * @param[in] t date à partir de laquelle on shift
+ * @param[in] h pas de différences finies
+ * @param[in] d indice du sous-jacent à shifter
+ * @param[in] timestep pas de constatation du sous-jacent
+ */
+void BlackScholesModel::shiftAsset(PnlMat *shift_path, const PnlMat *path, int d, double h, double t, double timestep) {
+    //TODO
+}
+
--- src/BlackScholesModel.h
+++ src/BlackScholesModel.h
@@ -12,9 +12,9 @@
 #ifndef PROJETMODPRO_BLACKSCHOLESMODEL_H
 #define PROJETMODPRO_BLACKSCHOLESMODEL_H
 
-#include<libaio.h>
-#include<vector>
-
+#include "pnl/pnl_random.h"
+#include "pnl/pnl_vector.h"
+#include "pnl/pnl_matrix.h"
 
 /**
  * \class BlackScholesModel
@@ -24,44 +24,157 @@
  */
 class BlackScholesModel {
 
+private:
+
+    /**
+     * \brief size_ correspond au nombre d'actifs du modèle.
+     *
+     */
+    int size_;
+
+    /**
+     * \brief r_ correspond au taux d'intérêt du zéro-coupon.
+     *
+     */
+    double r_;
+
+    /**
+     * \brief rho_ correspond au paramètre de corrélation du modèle.
+     *
+     */
+    double rho_;
+
+    /**
+     * \brief *sigma correspond au vecteur de volatilités.
+     *
+     */
+    PnlVect *sigma_;
+
+    /**
+     * \brief *spot_ correspond au vecteur de valeurs initiales des sous-jacents.
+     *
+     */
+    PnlVect *spot_;
+
 public:
 
     /**
-     * \brief firstSpots représente le vecteur des spots initiaux.
+     * \brief Getter de l'attribut \refitem size_
+     *
+     * @return l'attribut \refitem size_
+     *
      */
-    std::vector<float> firstSpots;
+    int getSize() const;
 
     /**
-     * \brief volatilities représente le vecteur des volatilités des sous-jacents.
+     * \brief Getter de l'attribut \refitem r_
+     *
+     * @return l'attribut \refitem r_
      *
      */
-    std::vector<float> volatilities;
+    double getR() const;
 
     /**
-     * \brief correlation représente la corrélation.
+     * \brief Getter de l'attribut \refitem rho_
+     *
+     * @return l'attribut \refitem rho_
      *
      */
-    float correlation;
+    double getRho() const;
 
     /**
-     * \brief interestRate représente le taux d'intérêt du zéro-coupon.
+     * \brief Getter de l'attribut \refitem sigma_
+     *
+     * @return l'attribut \refitem sigma_
      *
      */
-    float interestRate;
+    PnlVect *getSigma() const;
 
     /**
-     * \brief timestepNumber représente le nombre de discrétisations.
+     * \brief Getter de l'attrivut \refitem spot_
+     *
+     * @return l'attribut \refitem sigma_
      *
      */
-    int timestepNumber;
+    PnlVect *getSpot() const;
 
     /**
-     * \brief asset simule le marché sous le modèle de Black&Scholes avec les paramètres en attribut.
+     * \brief Setter de l'attribut \refitem size_
      *
-     * @return la matrice de taille (N+1)xD des spots aux différents temps des sous-jacents.
+     * @param size le nouveau nombre d'actifs du modèle, \refitem size_
      *
      */
-    void asset(float** &market);
+    void setSize(int size);
+
+    /**
+     * \brief Setter de l'attribut \refitem r_
+     *
+     * @param r le nouveau taux d'intérêt du ZC, \refitem r_
+     */
+    void setR(double r);
+
+    /**
+     * \brief Setter de l'attribut \refitem rho_
+     *
+     * @param rho la nouvelle corrélation du modèle, \refitem rho_
+     *
+     */
+    void setRho(double rho);
+
+    /**
+     * \brief Setter de l'attribut \refitem sigma_
+     *
+     * @param sigma le nouveau vecteur de volatilités du modèle, \refitem sigma_
+     *
+     */
+    void setSigma(PnlVect *sigma);
+
+    /**
+     * \brief Setter de l'attribut \refitem spot_
+     *
+     * @param spot le nouveau vecteur des spots initiaux du modèle, \refitem spot_
+     *
+     */
+    void setSpot(PnlVect *spot);
+
+    /**
+     * Génère une trajectoire du modèle et la stocke dans path
+     *
+     * @param[out] path contient une trajectoire du modèle.
+     * C'est une matrice de taille (nbTimeSteps+1) x d
+     * @param[in] T  maturité
+     * @param[in] nbTimeSteps nombre de dates de constatation
+     */
+    void asset(PnlMat *path, double T, int nbTimeSteps, PnlRng *rng);
+
+    /**
+     * Calcule une trajectoire du modèle connaissant le
+     * passé jusqu' à la date t
+     *
+     * @param[out] path  contient une trajectoire du sous-jacent
+     * donnée jusqu'à l'instant t par la matrice past
+     * @param[in] t date jusqu'à laquelle on connait la trajectoire.
+     * t n'est pas forcément une date de discrétisation
+     * @param[in] nbTimeSteps nombre de pas de constatation
+     * @param[in] T date jusqu'à laquelle on simule la trajectoire
+     * @param[in] past trajectoire réalisée jusqu'a la date t
+     */
+    void asset(PnlMat *path, double t, double T, int nbTimeSteps, PnlRng *rng, const PnlMat *past);
+
+    /**
+     * Shift d'une trajectoire du sous-jacent
+     *
+     * @param[in]  path contient en input la trajectoire
+     * du sous-jacent
+     * @param[out] shift_path contient la trajectoire path
+     * dont la composante d a été shiftée par (1+h)
+     * à partir de la date t.
+     * @param[in] t date à partir de laquelle on shift
+     * @param[in] h pas de différences finies
+     * @param[in] d indice du sous-jacent à shifter
+     * @param[in] timestep pas de constatation du sous-jacent
+     */
+    void shiftAsset(PnlMat *shift_path, const PnlMat *path, int d, double h, double t, double timestep);
 };
 
 
