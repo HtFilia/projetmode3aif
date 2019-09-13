c2abe97dadda1f3497c9c36058fbd23ced88305a
Raphaël Prêtre--Heckenroth <Raphael.Pretre--Heckenroth@grenoble-inp.org>
2019-09-13 11:33:14 +0200

    Google tests import dans cmake

---
+++ src/test1.cpp
@@ -0,0 +1,15 @@
+//
+// Created by pretrehr on 9/13/19.
+//
+
+#include "gtest/gtest.h"
+
+
+TEST(AbsoluteDateTestSuite, ExampleDate){
+    EXPECT_EQ(0,0);
+}
+
+
+TEST(AbsoluteDateTestSuite2, IncorrectDate){
+    EXPECT_EQ(0,0);
+}
