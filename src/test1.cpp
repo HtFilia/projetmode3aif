d2a11ab0032d47a827a156b389678a5c22ba6ffe
Raphaël Prêtre--Heckenroth <Raphael.Pretre--Heckenroth@grenoble-inp.org>
2019-09-16 10:57:43 +0200

    Modif Cmake pour compiler dans CLion

--- src/test1.cpp
+++ src/test1.cpp
@@ -8,18 +8,21 @@
 #include "pnl/pnl_matrix.h"
 
 
-TEST(Payoff, Example1){
+TEST(Payoff, Example1)
+{
     PnlVect* lambda = pnl_vect_create_from_scalar(1, 1);
     Basket* basketOption = new Basket(100, 10, 1, 1, lambda);
     PnlMat* path = pnl_mat_create_from_scalar(2, 1, 110);
     EXPECT_EQ(10,basketOption->payoff(path));
 }
 
-TEST(TestSuite1, Example2){
+TEST(MonteCarlo, Example1)
+{
     EXPECT_EQ(0,0);
 }
 
 
-TEST(TestSuite2, Example1){
+TEST(BlackScholes, Example1)
+{
     EXPECT_EQ(0,0);
 }
