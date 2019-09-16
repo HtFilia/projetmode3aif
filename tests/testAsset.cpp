3dcbe03926da4544f4862d431dba2c301da4382a
lucas <lucas.coute@grenoble-inp.org>
2019-09-16 12:08:33 +0200

    Merge i guess

--- tests/testAsset.cpp
+++ tests/testAsset.cpp
@@ -1,7 +1,7 @@
 /**
- * \file testPrice0.cpp
+ * \file testAsset.cpp
  *
- * \brief Fichier test de la méthode Price en 0 pour différentes options.
+ * \brief Fichier test de la méthode Asset pour différentes options.
  *
  * \authors LEBIHAN Lucas, COUTE Lucas, MOMMEJA Léonard, PRÊTRE-HECKENROTH Raphaël
  * Fait le 13.09.2019
@@ -12,31 +12,25 @@
 #include <iostream>
 #include <stdio.h>
 
-#include "Basket.hpp"
-#include "Asian.hpp"
-#include "Performance.hpp"
+#include "../src/BlackScholesModel.hpp"
 #include "pnl/pnl_vector.h"
 #include "pnl/pnl_matrix.h"
 
 int main(int argc, char *argv[]) {
-    int N = 1;
-    int d = 2;
-    double r = 1.1;
-    double rho = 0;
+    int N = 10;
+    int d = 5;
+    double r = 0.1;
+    double rho = 0.2;
     PnlVect *sigma = pnl_vect_create_from_scalar(d, 0.1);
     PnlVect *spot = pnl_vect_create_from_scalar(d, 100);
     BlackScholesModel *bsModel = new BlackScholesModel(d, r, rho, sigma, spot);
 
     PnlMat* path = pnl_mat_create(N+1, d);
-    double T = N / 252;
+    double T = 1;
     PnlRng *rng = pnl_rng_create(PNL_RNG_MERSENNE);
-
-    PnlVect* lambda = pnl_vect_create_from_scalar(d, 1);
-    Basket* basketOption = new Basket(K, T, d, N, lambda);
-
-    MonteCarlo monteCarlo = new MonteCarlo(bsModel, basketOption, rng, fdSteps, M);
+    pnl_rng_sseed(rng, time(NULL));
+    bsModel->asset(path, T, N, rng);
+    pnl_mat_print(path);
 
     return 0;
 }
-
-
--- tests/testPrice0.cpp
+++ tests/testPrice0.cpp
@@ -1,7 +1,7 @@
 /**
- * \file testAsset.cpp
+ * \file testPrice0.cpp
  *
- * \brief Fichier test de la méthode Asset pour différentes options.
+ * \brief Fichier test de la méthode Price en 0 pour différentes options.
  *
  * \authors LEBIHAN Lucas, COUTE Lucas, MOMMEJA Léonard, PRÊTRE-HECKENROTH Raphaël
  * Fait le 13.09.2019
@@ -12,25 +12,31 @@
 #include <iostream>
 #include <stdio.h>
 
-#include "BlackScholesModel.hpp"
+#include "../src/Basket.hpp"
+#include "../src/Asian.hpp"
+#include "../src/Performance.hpp"
 #include "pnl/pnl_vector.h"
 #include "pnl/pnl_matrix.h"
 
 int main(int argc, char *argv[]) {
-    int N = 10;
-    int d = 5;
-    double r = 0.1;
-    double rho = 0.2;
+    int N = 1;
+    int d = 2;
+    double r = 1.1;
+    double rho = 0;
     PnlVect *sigma = pnl_vect_create_from_scalar(d, 0.1);
     PnlVect *spot = pnl_vect_create_from_scalar(d, 100);
     BlackScholesModel *bsModel = new BlackScholesModel(d, r, rho, sigma, spot);
 
     PnlMat* path = pnl_mat_create(N+1, d);
-    double T = 1;
+    double T = N / 252;
     PnlRng *rng = pnl_rng_create(PNL_RNG_MERSENNE);
-    pnl_rng_sseed(rng, time(NULL));
-    bsModel->asset(path, T, N, rng);
-    pnl_mat_print(path);
+
+    PnlVect* lambda = pnl_vect_create_from_scalar(d, 1);
+    Basket* basketOption = new Basket(K, T, d, N, lambda);
+
+    MonteCarlo monteCarlo = new MonteCarlo(bsModel, basketOption, rng, fdSteps, M);
 
     return 0;
 }
+
+
