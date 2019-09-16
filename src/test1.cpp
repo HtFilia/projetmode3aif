f3e46cbbc772bdf4d0af8522e88d36d68324df13
Raphaël Prêtre--Heckenroth <Raphael.Pretre--Heckenroth@grenoble-inp.org>
2019-09-16 10:10:04 +0200

    test payoff auto

--- src/test1.cpp
+++ src/test1.cpp
@@ -3,10 +3,16 @@
 //
 
 #include "gtest/gtest.h"
+#include "Basket.hpp"
+#include "pnl/pnl_vector.h"
+#include "pnl/pnl_matrix.h"
 
 
-TEST(TestSuite1, Example1){
-    EXPECT_EQ(0,0);
+TEST(Payoff, Example1){
+    PnlVect* lambda = pnl_vect_create_from_scalar(1, 1);
+    Basket* basketOption = new Basket(100, 10, 1, 1, lambda);
+    PnlMat* path = pnl_mat_create_from_scalar(2, 1, 110);
+    EXPECT_EQ(10,basketOption->payoff(path));
 }
 
 TEST(TestSuite1, Example2){
