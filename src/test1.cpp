3803c4e9013601d4d36e1322b86f15cdeff4c6f0
Raphaël Prêtre--Heckenroth <Raphael.Pretre--Heckenroth@grenoble-inp.org>
2019-09-16 12:05:36 +0200

    parsing test

--- src/test1.cpp
+++ src/test1.cpp
@@ -2,11 +2,15 @@
 // Created by pretrehr on 9/13/19.
 //
 
+#include <string>
+
 #include "gtest/gtest.h"
 #include "Basket.hpp"
 #include "pnl/pnl_vector.h"
 #include "pnl/pnl_matrix.h"
+#include "jlparser/parser.hpp"
 
+using namespace std;
 
 TEST(Payoff, Example1)
 {
@@ -15,6 +19,42 @@ TEST(Payoff, Example1)
     PnlMat* path = pnl_mat_create_from_scalar(2, 1, 110);
     EXPECT_EQ(10,basketOption->payoff(path));
 }
+
+TEST(Payoff, ExampleParsing)
+{
+    double T, r, strike;
+    PnlVect *spot, *sigma, *divid, *payoff_coefficients;
+    string type;
+    int size;
+    size_t n_samples;
+
+    string str = "../produits/basket.dat";
+    const char *infile = str.c_str();
+    Param *P = new Parser(infile);
+
+    P->extract("option type", type);
+    P->extract("maturity", T);
+    P->extract("option size", size);
+    P->extract("spot", spot, size);
+    P->extract("volatility", sigma, size);
+    P->extract("interest rate", r);
+    if (P->extract("dividend rate", divid, size, true) == false)
+    {
+        divid = pnl_vect_create_from_zero(size);
+    }
+    P->extract("strike", strike);
+    P->extract("sample number", n_samples);
+    P->extract("payoff coefficients", payoff_coefficients, 1);
+    Basket* basketOption = new Basket(strike, T, size, n_samples, payoff_coefficients);
+    PnlMat* path = pnl_mat_create_from_scalar(2, 1, 110);
+    cout << basketOption->payoff(path) << endl;
+    pnl_vect_free(&spot);
+    pnl_vect_free(&sigma);
+    pnl_vect_free(&divid);
+
+    delete P;
+}
+
 
 TEST(MonteCarlo, Example1)
 {
