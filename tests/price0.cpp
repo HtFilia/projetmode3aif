f0cfb9ad57ebc7c10e67ccb9d3ad19937ca3a199
Lucas Lebihan <Lucas.Lebihan@ensimag.grenoble-inp.fr>
2019-09-19 13:29:27 +0200

    Continuite price0 debug en cours

--- tests/price0.cpp
+++ tests/price0.cpp
@@ -10,6 +10,7 @@
  */
 
 #include <iostream>
+#include <stdio.h>
 #include <string>
 
 #include "jlparser/parser.hpp"
@@ -25,23 +26,36 @@
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
+int main(int argc, char **argv) {
 
     // Resultat variables
     double prix, prix_std_dev;
     PnlVect* delta, *delta_std_dev;
 
-    // Option
-    double T, r, strike, payoffCoeff;
-    PnlVect *spot, *sigma, *divid;
+    // Option variables
+    double T, strike, payoffCoeff;
+    PnlVect *divid, *lambda;
     string type;
     int size;
     int timestepNumber;
-    size_t n_samples;
     Option *option;
+
+    // BSModel variables
+    int dim;
+    double r, rho;
+    PnlVect *spot, *sigma;
+
+    // MonteCarlo variables
+    size_t n_samples;
+    PnlRng *rng = pnl_rng_create(PNL_RNG_MERSENNE);
+    pnl_rng_sseed(rng, 1);
+    double fdSteps;
+
+    // Parser and file to parse
     char *infile = argv[1];
     Param *P = new Parser(infile);
 
+    // Parsing
     P->extract("option type", type);
     P->extract("maturity", T);
     P->extract("option size", size);
@@ -55,11 +69,11 @@ int main(int argc, char* argv[]) {
     P->extract("strike", strike);
     P->extract("sample number", n_samples);
     P->extract("timestep number", timestepNumber);
-    //P->extract("payoff coefficients", payoffCoeff);
+    P->extract("payoff coefficients", lambda, size);
+    P->extract("sample number", n_samples);
+    fdSteps = T / n_samples;
 
-    PnlVect* lambda;
-    // pnl_vect_create_from_scalar(size, payoffCoeff);
-
+    // Create Option according to parameters
     if (type == "basket") {
         option = new Basket(strike, T, size, timestepNumber, lambda);
     } else if (type == "asian") {
@@ -68,18 +82,33 @@ int main(int argc, char* argv[]) {
         option = new Performance(T, size, timestepNumber, lambda);
     }
 
+    // Create BSModel according to parameters
+    BlackScholesModel *bsModel = new BlackScholesModel(dim, r, rho, sigma, spot);
 
+    // Create Market according to parameters
+    PnlMat *path = pnl_mat_create(timestepNumber + 1, dim);
+    bsModel->asset(path, T, timestepNumber, rng);
+    pnl_rng_sseed(rng, time(NULL));
 
+    // Create MonteCarlo according to parameters
+    MonteCarlo *monteCarlo = new MonteCarlo(bsModel, option, rng, fdSteps, n_samples);
 
-    //TODO
-    //PricingResults res(prix, prix_std_dev, delta, delta_std_dev);
-    //std::cout << res << std::endl;
+    // Get price, deltas and st.deviation
+    monteCarlo->price(prix, prix_std_dev);
+    monteCarlo->delta(path, 0, delta, delta_std_dev);
+
+    // Print Results
+    PricingResults res(prix, prix_std_dev, delta, delta_std_dev);
+    std::cout << res << std::endl;
 
     //free
     pnl_vect_free(&spot);
     pnl_vect_free(&sigma);
     pnl_vect_free(&divid);
+    pnl_rng_free(&rng);
     delete P;
-
+    delete option;
+    delete bsModel;
+    delete monteCarlo;
     return 0;
 }
